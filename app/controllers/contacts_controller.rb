bd70c1e02b9d1ce1c662e39e12bd6a4017000591
Vitor Leepkaln <vitorpacheco@live.com>
2017-07-04 14:25:51 +0000

    Added form validations to model.

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -8,7 +8,7 @@ class ContactsController < ApplicationController
     if @contact.save 
       redirect_to new_contact_path, notice: "Message sent."
     else
-      redirect_to new_contact_path, notice: "Error ocurred."
+      redirect_to new_contact_path, notice: "An error ocurred."
     end
   end
   
